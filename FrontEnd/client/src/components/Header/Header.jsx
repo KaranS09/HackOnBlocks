991e73bafca6a0b5be7f7188e56bd732eb275bb8
Shashwat0227 <shashwatchaturvedig@gmail.com>
2024-07-18 00:00:26 +0530

    Headers done

--- FrontEnd/client/src/components/Header/Header.jsx
+++ FrontEnd/client/src/components/Header/Header.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import "./header.css";
+import { Container } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
+
+// Nav links
+
+const NAV__LINKS = [
+  {
+    display: "Login",
+    url: "/login",
+  },
+
+  {
+    display: "Home",
+    url: "/home",
+  },
+  {
+    display: "Market",
+    url: "/market",
+  },
+  {
+    display: "Create",
+    url: "/create",
+  },
+  {
+    display: "Contact",
+    url: "/contact",
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation">
+          <div className="logo">
+            <h2 className="d-flex gap-2 align-items-center">
+              <span>
+                <i class="ri-safe-2-fill"></i>
+              </span>
+              CreDefi
+            </h2>
+          </div>
+
+          <div className="nav__menu">
+            <ul className="nav__list">
+              {NAV__LINKS.map((item, index) => (
+                <li className="nav__item" key={index}>
+                  <NavLink to={item.url}>{item.display}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="nav__right  d-flex align-items-center gap-5">
+            <button className="btn">
+              <Link to="/wallet" className="d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-wallet-line"></i>
+                </span>
+                Connect Wallet
+              </Link>
+            </button>
+
+            <span className="mobile__menu">
+              <i class="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
--- FrontEnd/client/src/index.js
+++ FrontEnd/client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "remixicon/fonts/remixicon.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
